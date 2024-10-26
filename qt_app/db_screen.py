25cb9360f728c5c355d9b21336fb324b7fcdad5e
Nikita Golubkov <golubkoffnickita0@gmail.com>
2024-10-27 01:05:17 +0300

    feat: add table QT window

--- qt_app/db_screen.py
+++ qt_app/db_screen.py
@@ -1,6 +1,7 @@
 from PyQt6 import QtWidgets
 import sys
 from qt_app.user_interfaces.sports_interface import Ui_MainWindow
+from table_window_screen import table_window
 
 
 class db_screen(Ui_MainWindow, QtWidgets.QMainWindow):
@@ -9,6 +10,17 @@ class db_screen(Ui_MainWindow, QtWidgets.QMainWindow):
 
         self.setupUi(self)
         self.setup_btns()
+        self.data_existing = True
+
+        try:
+            self.tabled_db = table_window()
+
+        except FileNotFoundError:
+            self.statusbar.setStyleSheet('color: red')
+            self.statusbar.showMessage('Database json does not exist')
+            self.data_existing = False
+
+
 
     def setup_btns(self):
         self.Hockey.clicked.connect(self.submit)
@@ -18,7 +30,11 @@ class db_screen(Ui_MainWindow, QtWidgets.QMainWindow):
         self.Bascketball.clicked.connect(self.submit)
 
     def submit(self):
-        pass
+        if self.sender() == self.Football and self.data_existing:
+                self.tabled_db.ui_create()
+                self.tabled_db.show()
+                self.close()
+
 
 
 if __name__ == "__main__":
---
+++ qt_app/table_window_screen.py
@@ -0,0 +1,79 @@
+from PyQt6 import QtCore, QtGui, QtWidgets
+import sys
+
+from PyQt6.QtWidgets import QLabel, QPushButton
+
+from qt_app.user_interfaces.table_window_interface import Ui_MainWindow
+import json
+
+
+class table_window(Ui_MainWindow, QtWidgets.QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+        self.setupUi(self)
+
+        self.correct_table = 'awards'
+
+        self.file = open('json_files/database.json', 'r')
+        self.data = json.load(self.file)
+
+        self.mainTable.cellDoubleClicked.connect(self.change_table)
+
+
+        self.team_words = ['team_name', 'team_id', 'team1_id', 'team2_id', 'winner_team_id','champion_team_id']
+        self.game_words = ['game_id']
+        self.judge_words = ['judge_id']
+        self.season_words = ['season_id']
+
+
+
+    def ui_create(self) -> None:
+        self.mainTable.clear()
+        data = self.data[self.correct_table]
+        self.mainTable.setRowCount(len(data))
+        self.mainTable.setColumnCount(len(data[0]))
+        self.mainTable.setHorizontalHeaderLabels(list(map(lambda x: x.capitalize().replace('_', ' '),
+                                                              (data[0].keys()))))
+
+        for x_coord, row in enumerate(data):
+            for y_coord, cell in enumerate(row.values()):
+                label = QLabel()
+                label.setText(str(cell))
+                self.mainTable.setCellWidget(x_coord, y_coord, label)
+        self.mainTable.resizeColumnsToContents()
+
+
+    def change_table(self, x, y):
+        data = self.data[self.correct_table]
+        changing = list(data[0].keys())[y]
+        print(changing)
+
+        if str(changing) in self.team_words:
+            self.correct_table = 'teams'
+
+        elif str(changing) in self.game_words:
+            self.correct_table = 'games'
+
+        elif str(changing) in self.judge_words:
+            self.correct_table = 'judges'
+
+        elif str(changing) in self.season_words:
+            self.correct_table = 'seasons'
+
+        self.ui_create()
+
+
+if __name__ == "__main__":
+    app = QtWidgets.QApplication(sys.argv)
+    try:
+        window = table_window()
+        window.ui_create()
+
+        window.show()
+        app.exec()
+
+    except FileNotFoundError:
+        pass
+
+
